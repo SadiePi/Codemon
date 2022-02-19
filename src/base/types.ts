4f69b5336b5b583b2d4eef4b36306ef1a2239be4
Sadie Dotzler <sadiedotzler@gmail.com>
2022-02-18 17:22:03 -0700

    add name property to types

--- src/base/types.ts
+++ src/base/types.ts
@@ -21,54 +21,63 @@ let Dark: Type = INIT;
 let Fairy: Type = INIT;
 
 Normal = {
+  name: "Normal",
   weaknesses: [Fighting],
   resistances: [],
   immunities: [Ghost],
 };
 
 Fighting = {
+  name: "Fighting",
   weaknesses: [Flying, Psychic, Fairy],
   resistances: [Rock, Bug],
   immunities: [],
 };
 
 Flying = {
+  name: "Flying",
   weaknesses: [Rock, Electric, Ice],
   resistances: [Fighting, Bug, Grass],
   immunities: [Ground],
 };
 
 Poison = {
+  name: "Poison",
   weaknesses: [Ground, Psychic],
   resistances: [Fighting, Poison, Bug, Grass, Fairy],
   immunities: [],
 };
 
 Ground = {
+  name: "Ground",
   weaknesses: [Water, Grass, Ice],
   resistances: [Poison, Rock],
   immunities: [Electric],
 };
 
 Rock = {
+  name: "Rock",
   weaknesses: [Fighting, Ground, Steel, Water, Grass],
   resistances: [Normal, Flying, Poison, Fire],
   immunities: [],
 };
 
 Bug = {
+  name: "Bug",
   weaknesses: [Flying, Rock, Fire],
   resistances: [Fighting, Ground, Grass],
   immunities: [],
 };
 
 Ghost = {
+  name: "Ghost",
   weaknesses: [Ghost, Dark],
   resistances: [Poison, Bug],
   immunities: [Normal, Fighting],
 };
 
 Steel = {
+  name: "Steel",
   weaknesses: [Fighting, Ground, Fire],
   resistances: [
     Normal,
@@ -86,54 +95,63 @@ Steel = {
 };
 
 Fire = {
+  name: "Fire",
   weaknesses: [Ground, Rock, Water],
   resistances: [Bug, Steel, Fire, Grass, Ice, Fairy],
   immunities: [],
 };
 
 Water = {
+  name: "Water",
   weaknesses: [Grass, Electric],
   resistances: [Steel, Fire, Water],
   immunities: [],
 };
 
 Grass = {
+  name: "Grass",
   weaknesses: [Flying, Poison, Bug, Fire, Ice],
   resistances: [Ground, Water, Grass, Electric],
   immunities: [],
 };
 
 Electric = {
+  name: "Electric",
   weaknesses: [Ground],
   resistances: [Flying, Steel, Electric],
   immunities: [],
 };
 
 Psychic = {
+  name: "Psychic",
   weaknesses: [Bug, Ghost, Dark],
   resistances: [Fighting, Psychic],
   immunities: [],
 };
 
 Ice = {
+  name: "Ice",
   weaknesses: [Fighting, Rock, Steel, Fire],
   resistances: [Ice],
   immunities: [],
 };
 
 Dragon = {
+  name: "Dragon",
   weaknesses: [Ice, Dragon, Fairy],
   resistances: [Fire, Water, Grass, Electric],
   immunities: [],
 };
 
 Dark = {
+  name: "Dark",
   weaknesses: [Fighting, Bug, Fairy],
   resistances: [Ghost, Dark],
   immunities: [Psychic],
 };
 
 Fairy = {
+  name: "Fairy",
   weaknesses: [Poison, Steel],
   resistances: [Fighting, Bug, Dark],
   immunities: [Dragon],
--- src/core/type.ts
+++ src/core/type.ts
@@ -1,10 +1,12 @@
 export interface Type {
+  name: string;
   weaknesses: Type[];
   resistances: Type[];
   immunities: Type[];
 }
 
 export const INIT: Type = {
+  name: "!!! INIT !!!",
   weaknesses: [],
   resistances: [],
   immunities: [],
