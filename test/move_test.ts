4e09af277f4afc92ffc75ef9be668b5ce515d1ec
Sadie Dotzler <sadiedotzler@gmail.com>
2022-02-18 22:05:16 -0700

    fixes, tackle usage test

--- test/move_test.ts
+++ test/move_test.ts
@@ -3,22 +3,17 @@ import {
   assertNotEquals,
 } from "https://deno.land/std/testing/asserts.ts";
 
-import C, {
-  DamageCategory,
-  MoveInfo,
-  Move,
-  TC,
-  Codemon,
-} from "../src/base/index.ts";
+import C, { Move } from "../src/base/index.ts";
+import { chompy } from "./codemon_test.ts";
 
 Deno.test("Tackle basic info", () => {
-  const Tackle1 = new Move({} as Codemon, C.Moves.Tackle);
+  const Tackle1 = new Move({ self: chompy, info: C.Moves.Tackle });
 
-  assertEquals(Tackle1.args.type.immunities.length, 1, "Wrong immunity count");
-  assertEquals(Tackle1.args.type.immunities[0].name, "Ghost", "Wrong immunity");
-  assertEquals(Tackle1.args.type.weaknesses.length, 1, "Wrong weakness count");
+  assertEquals(Tackle1.info.type.immunities.length, 1, "Wrong immunity count");
+  assertEquals(Tackle1.info.type.immunities[0].name, "Ghost", "Wrong immunity");
+  assertEquals(Tackle1.info.type.weaknesses.length, 1, "Wrong weakness count");
   assertEquals(
-    Tackle1.args.type.weaknesses[0].name,
+    Tackle1.info.type.weaknesses[0].name,
     "Fighting",
     "Wrong weakness"
   );
@@ -27,4 +22,17 @@ Deno.test("Tackle basic info", () => {
   assertNotEquals(Tackle1.PP.Boost(), 0, "Boost 3 failed");
   assertEquals(Tackle1.PP.Boost(), 0, "Boost 4 didn't fail");
   assertEquals(Tackle1.PP.max, 56, `Max PP is ${Tackle1.PP.max}, not 56`);
+});
+
+Deno.test("Tackle usage", () => {
+  console.log();
+
+  console.log(chompy.stats.HP.current);
+  console.log(chompy.moves);
+  const mu = chompy.moves[0].Use(chompy, false);
+  mu.random = 0.85;
+  console.log(mu);
+  const ur = chompy.RecieveMove(chompy, mu);
+  console.log(ur.damage);
+  console.log(chompy.stats.HP.current);
 });
