52439a02a68af554173fd0ece50aa034cecf87e0
Sadie Dotzler <sadiedotzler@gmail.com>
2022-02-18 22:01:50 -0700

    fixes

--- src/base/moves.ts
+++ src/base/moves.ts
@@ -1,15 +1,16 @@
-import C, { DamageCategory, MoveInfo, TC } from "./index.ts";
+import { MoveInfo, TC } from "./index.ts";
+import Types from "./types.ts";
 
 export const Tackle: MoveInfo = {
   name: "Tackle",
-  type: C.Types.Normal,
+  type: Types.Normal,
   basePP: 35, // max 56
   basePower: 40,
   priority: 0,
   baseAccuracy: 100,
   makesContact: true,
   criticalHitStage: 0,
-  damageCategory: DamageCategory.Physical,
+  damageCategory: "Physical",
   criticalHitProbabilityMultiplier: 1,
   targetingCategory: TC.Adjacent,
 };
