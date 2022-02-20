e77ac66455f393d384cda3765729dfdcc22dcc56
Sadie Dotzler <sadiedotzler@gmail.com>
2022-02-20 01:38:52 -0700

    rudimentary turn and move control

--- src/core/battle.ts
+++ src/core/battle.ts
@@ -7,10 +7,43 @@ async function sleep(ms: number) {
   await new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-export class WildBattle {
-  constructor(public left: Codemon, public right: Codemon) {}
-  public async play() {
-    console.log("Playing battle:", this);
-    await sleep(5000);
+export function wildBattle(left: Codemon, right: Codemon) {
+  console.log(`Playing battle between ${left.name} and ${right.name}`);
+  console.log(left.toString());
+  console.log(right.toString());
+
+  // TODO: consider move priority
+  while (left.stats.hp.current > 0 && right.stats.hp.current > 0) {
+    // determine the faster mon
+    const leftSpeed = left.stats.speed.value(true);
+    const rightSpeed = right.stats.speed.value(true);
+    let first = left,
+      second = right;
+    if (leftSpeed < rightSpeed) {
+      first = right;
+      second = left;
+    }
+
+    console.log(`${first.name} will go first, then ${second.name}`);
+
+    // have both use moves (for now just the first move in the array)
+    console.log(first.toString());
+    attack(first, second);
+    console.log(second.toString());
+    attack(second, first);
   }
+
+  let winner = left;
+  if (right.stats.hp.current > 0) winner = right;
+
+  console.log(`${winner.name} won!`);
+}
+
+export function attack(attacker: Codemon, target: Codemon) {
+  if (attacker.stats.hp.current <= 0) return;
+
+  const usage = attacker.moves[0].Use(target, false);
+  console.log(`${attacker.name} used ${usage.info.name}!`);
+  const hit = target.RecieveMove(usage);
+  console.log(`It did ${hit.damage} damage!`);
 }
--- test/battle_test.ts
+++ test/battle_test.ts
@@ -1,17 +1,30 @@
 import C, { Codemon } from "../src/base/index.ts";
-import { WildBattle } from "../src/core/battle.ts";
+import { wildBattle } from "../src/core/battle.ts";
 
 Deno.test({
   name: "WildBattle",
   fn: async () => {
     const bulby = new Codemon({
       species: C.Species.Bulbasaur,
+      name: "Bulby",
       moves: [C.Moves.Tackle],
+      level: 50,
     });
     const chompy = new Codemon({
       species: C.Species.Garchomp,
+      name: "Chompy",
       moves: [C.Moves.Tackle],
+      stats: {
+        hp: {
+          effortValue: 80,
+        },
+        defense: {
+          individualValue: 10,
+          effortValue: 50,
+        },
+      },
+      level: 15,
     });
-    await new WildBattle(bulby, chompy).play();
+    wildBattle(bulby, chompy);
   },
 });
