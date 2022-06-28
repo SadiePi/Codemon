704778f86bfa0e3e815538a4e0d0606af5c83a7f
Sadie Dotzler <sadiedotzler@gmail.com>
2022-06-28 02:12:10 -0700

    idea for mods

--- src/core/index.ts
+++ src/core/index.ts
@@ -4,6 +4,7 @@ export * from "./codemon.ts";
 export * from "./config.ts";
 export * from "./experience.ts";
 export * from "./item.ts";
+export * from "./mod.ts";
 export * from "./move.ts";
 export * from "./nature.ts";
 export * from "./sex.ts";
@@ -11,3 +12,6 @@ export * from "./species.ts";
 export * from "./stats.ts";
 export * from "./status.ts";
 export * from "./type.ts";
+
+import { Load } from "./mod.ts";
+export default Load;
