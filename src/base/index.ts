7004469dea83a59f79665e3fa243139f04593e1a
Sadie Dotzler <sadiedotzler@gmail.com>
2022-07-02 02:37:32 -0700

    cleanup and planning

--- src/base/index.ts
+++ src/base/index.ts
@@ -1,16 +1,24 @@
 export * from "../core/index.ts";
 
 export * from "./battle.ts";
-import Experience from "./experience.ts";
 import Species from "./species.ts";
+import Moves from "./moves.ts";
 import Types from "./types.ts";
 import Nature from "./nature.ts";
-import Moves from "./moves.ts";
+import Experience from "./experience.ts";
 
+// Default export acts as a Codex, this library's equivalent of the Pokedex.
+// Except with waaaaay more data than the Pokedex ever revealed.
 export default {
-  Experience,
   Species,
+  Moves,
   Types,
   Nature,
-  Moves,
+  //Abilities,
+  //Graphics,
+  //BodyStyles,
+  //Footprints,
+  //CodexColors,
+  Experience,
+  //Items,
 };
