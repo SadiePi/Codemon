677f46e5919adb8f9cf74e787e798a9948bb43a1
Sadie Dotzler <sadiedotzler@gmail.com>
2023-09-13 00:59:10 -0700

    preliminary items file

--- codex/uranium/index.ts
+++ codex/uranium/index.ts
@@ -3,17 +3,21 @@ import { Codex } from "../../src/mod.ts";
 import loader from "./loader.ts";
 
 import Pokedex from "../pokemon/mod.ts";
+import * as Abilities from "./abilities.ts";
+import * as Items from "./items.ts";
+import * as Moves from "./moves.ts";
 import * as Species from "./species.ts";
 import * as Types from "./types.ts";
-import * as Abilities from "./abilities.ts";
-import * as Moves from "./moves.ts";
+import * as Weather from "./weather.ts";
 
 const U = {
   ...Pokedex,
+  Abilities: { ...Abilities, ...Pokedex.Abilities },
+  Items: { ...Items, ...Pokedex.Items },
+  Moves: { ...Moves, ...Pokedex.Moves },
   Species: { ...Species, ...Pokedex.Species },
   Types: { ...Types, ...Pokedex.Types },
-  Abilities: { ...Abilities, ...Pokedex.Abilities },
-  Moves: { ...Moves, ...Pokedex.Moves },
+  Weathers: { ...Weather, ...Pokedex.Weathers },
 } as const satisfies Codex;
 export type UraniumCodex = typeof U;
 
