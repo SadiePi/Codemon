22680a89f54e4fa30c14bc03a7c3d6b3fd72da11
Sadie Dotzler <sadiedotzler@gmail.com>
2023-10-06 07:07:42 -0700

    full Rough Skin implenentation

--- codex/pokemon/abilities/roughSkin.ts
+++ codex/pokemon/abilities/roughSkin.ts
@@ -1,4 +1,33 @@
-import { Ability } from "../mod.ts";
-import loader from "../loader.ts";
+import { Ability, MoveEntry, TargetContext, effectAction, permanent, TraditionalBBP as T, Effects } from "../mod.ts";
 
-export const RoughSkin = {} as Ability;
+export const RoughSkin: Ability = {
+  name: "Rough Skin",
+  description: "The Pokémon's rough skin damages attackers that make direct contact with it.",
+  slot: "ability",
+
+  apply: ({ self }) => {
+    function damageAttackerOnContact(_effect: Effects<T>, context: TargetContext<T>) {
+      if (context.target !== self) return;
+      const { action } = context;
+      if (!(action.params.source instanceof MoveEntry)) return;
+      if (!action.params.source.effects.makesContact) return;
+
+      action.reactions.add(
+        effectAction(context.battle, action.params.source.user, {
+          hp: -Math.floor(action.params.source.user.stats.hp.max / 8),
+        })
+      );
+    }
+
+    return {
+      name: RoughSkin.name,
+      activate: () => {
+        self.on("receiveEffects", damageAttackerOnContact);
+      },
+      deactivate: () => {
+        self.off("receiveEffects", damageAttackerOnContact);
+      },
+      expiry: permanent,
+    };
+  },
+};
