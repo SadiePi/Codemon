234abb411558b2ef6f26cf158283b7d5ced6d032
Sadie Dotzler <sadiedotzler@gmail.com>
2023-10-11 16:54:27 -0700

    rename Pokedex import

--- test/codemon_test.ts
+++ test/codemon_test.ts
@@ -1,22 +1,22 @@
 import { assert, iBulby, iKibble } from "./common.ts";
 import { spawn } from "../src/mod.ts";
-import C from "../codex/pokemon/mod.ts";
+import P from "../codex/pokemon/mod.ts";
 
 Deno.test("Basics", () => {
   const bulby = spawn(iBulby);
 
-  assert(bulby.getSpecies(false) === C.Species.Bulbasaur);
+  assert(bulby.getSpecies(false) === P.Species.Bulbasaur);
   assert(bulby.name, "Bulby");
-  assert(bulby.gender === C.Genders.Male);
-  assert(bulby.nature === C.Natures.Quiet);
+  assert(bulby.gender === P.Genders.Male);
+  assert(bulby.nature === P.Natures.Quiet);
   assert(bulby.stats.level === 15);
   // TODO compare stats to expected values
   assert(bulby.moves.length === 4);
-  assert(bulby.moves[0].effects === C.Moves.Tackle);
-  assert(bulby.moves[1].effects === C.Moves.RazorLeaf);
-  assert(bulby.moves[2].effects === C.Moves.StunSpore);
-  assert(bulby.moves[3].effects === C.Moves.SolarBeam);
-  assert(bulby.ability === C.Abilities.Overgrow);
+  assert(bulby.moves[0].effects === P.Moves.Tackle);
+  assert(bulby.moves[1].effects === P.Moves.RazorLeaf);
+  assert(bulby.moves[2].effects === P.Moves.StunSpore);
+  assert(bulby.moves[3].effects === P.Moves.SolarBeam);
+  assert(bulby.ability === P.Abilities.Overgrow);
 });
 
 // Vaguely in the same order as the class's code
@@ -30,10 +30,10 @@ Deno.test("Species", () => {
   assert(species.description === "Mutation test");
   assert(species.name === "Mutated Bulbasaur");
 
-  bulby.mutate({ types: [C.Types.Ghost, C.Types.Fire] });
+  bulby.mutate({ types: [P.Types.Ghost, P.Types.Fire] });
   species = bulby.getSpecies();
-  assert(species.types[0] === C.Types.Ghost);
-  assert(species.types[1] === C.Types.Fire);
+  assert(species.types[0] === P.Types.Ghost);
+  assert(species.types[1] === P.Types.Fire);
   assert(species.name === "Mutated Bulbasaur");
 
   bulby.mutate({ name: "Mutation Test Bulbasaur" });
@@ -44,22 +44,22 @@ Deno.test("Species", () => {
   const originalSpecies = bulby.getSpecies(false);
   assert(originalSpecies.name === "Bulbasaur");
   assert(originalSpecies.description !== "Mutation test");
-  assert(originalSpecies.types[0] === C.Types.Grass);
-  assert(originalSpecies.types[1] === C.Types.Poison);
+  assert(originalSpecies.types[0] === P.Types.Grass);
+  assert(originalSpecies.types[1] === P.Types.Poison);
 
-  bulby.setSpecies(C.Species.Ivysaur); // should retain mutations
+  bulby.setSpecies(P.Species.Ivysaur); // should retain mutations
   species = bulby.getSpecies();
   assert(species.name === "Mutation Test Bulbasaur");
   assert(species.description === "Mutation test");
-  assert(species.types[0] === C.Types.Ghost);
-  assert(species.types[1] === C.Types.Fire);
+  assert(species.types[0] === P.Types.Ghost);
+  assert(species.types[1] === P.Types.Fire);
 
-  bulby.setSpecies(C.Species.Bulbasaur, false); // should reset mutations
+  bulby.setSpecies(P.Species.Bulbasaur, false); // should reset mutations
   species = bulby.getSpecies();
   assert(species.name === "Bulbasaur");
   assert(species.description !== "Mutation test");
-  assert(species.types[0] === C.Types.Grass);
-  assert(species.types[1] === C.Types.Poison);
+  assert(species.types[0] === P.Types.Grass);
+  assert(species.types[1] === P.Types.Poison);
 });
 Deno.test("Moves", () => {});
 Deno.test("Stats", () => {
@@ -75,29 +75,29 @@ Deno.test("Stats", () => {
 });
 Deno.test("Abilities", () => {
   const bulby = spawn(iBulby);
-  assert(bulby.ability === C.Abilities.Overgrow);
-  assert(bulby.originalAbility === C.Abilities.Overgrow);
+  assert(bulby.ability === P.Abilities.Overgrow);
+  assert(bulby.originalAbility === P.Abilities.Overgrow);
 
   // TODO use a species with multiple abilities
 
   bulby.ability = "hidden";
-  assert(bulby.ability === C.Abilities.Chlorophyll);
-  assert(bulby.originalAbility === C.Abilities.Overgrow);
+  assert(bulby.ability === P.Abilities.Chlorophyll);
+  assert(bulby.originalAbility === P.Abilities.Overgrow);
 
-  bulby.ability = C.Abilities.Adaptability;
-  assert(bulby.ability === C.Abilities.Adaptability);
-  assert(bulby.originalAbility === C.Abilities.Overgrow);
+  bulby.ability = P.Abilities.Adaptability;
+  assert(bulby.ability === P.Abilities.Adaptability);
+  assert(bulby.originalAbility === P.Abilities.Overgrow);
 
-  bulby.originalAbility = C.Abilities.Aerilate;
-  assert(bulby.ability === C.Abilities.Adaptability);
-  assert(bulby.originalAbility === C.Abilities.Aerilate);
+  bulby.originalAbility = P.Abilities.Aerilate;
+  assert(bulby.ability === P.Abilities.Adaptability);
+  assert(bulby.originalAbility === P.Abilities.Aerilate);
 
   bulby.resetAbility();
-  assert(bulby.ability === C.Abilities.Aerilate);
+  assert(bulby.ability === P.Abilities.Aerilate);
 
   bulby.ability = 0;
-  assert(bulby.ability === C.Abilities.Overgrow);
-  assert(bulby.originalAbility === C.Abilities.Aerilate);
+  assert(bulby.ability === P.Abilities.Overgrow);
+  assert(bulby.originalAbility === P.Abilities.Aerilate);
 });
 Deno.test("Natures", () => {});
 Deno.test("Names", () => {});
