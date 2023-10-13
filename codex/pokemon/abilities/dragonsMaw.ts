1c7b35505befae8976a50e8b8287ac0a3b607b7f
Sadie Dotzler <sadiedotzler@gmail.com>
2023-10-12 20:02:19 -0700

    implement dragon's maw

--- codex/pokemon/abilities/dragonsMaw.ts
+++ codex/pokemon/abilities/dragonsMaw.ts
@@ -1,3 +1,25 @@
-import { Ability } from "../mod.ts";
+import P, { Ability, Effects, permanent, proxy, TraditionalBBP as T } from "../mod.ts";
 
-export const DragonsMaw: Ability = {} as Ability;
+export const DragonsMaw: Ability = {
+  name: "Dragon's Maw", // OwO
+  description: "Powers up Dragon-type moves",
+  slot: "ability",
+
+  apply: ({ self }) => {
+    function boostDragonMoves(effect: Effects<T>) {
+      if (!effect.attack) return;
+
+      effect.attack = proxy(effect.attack, result => {
+        if (!result) return;
+        if (result && result.type == P.Types.Dragon) result.stat *= 1.5;
+      });
+    }
+
+    return {
+      name: DragonsMaw.name,
+      activate: () => self.on("inflictEffects", boostDragonMoves),
+      deactivate: () => self.off("inflictEffects", boostDragonMoves),
+      expiry: permanent,
+    };
+  },
+};
