d24eeb4a38ace40a3f23061d845c87f14f2f9009
Sadie Dotzler <sadiedotzler@gmail.com>
2023-10-12 19:12:13 -0700

    implement damp

--- codex/pokemon/abilities/damp.ts
+++ codex/pokemon/abilities/damp.ts
@@ -1,4 +1,34 @@
-import { Ability } from "../mod.ts";
-import loader from "../loader.ts";
+import P, { Ability, TraditionalBBP as T, permanent, MoveEntry, Action, Battle } from "../mod.ts";
 
-export const Damp: Ability = {} as Ability;
+// despite the general description, Damp specifically prevents these move:
+const DampMoves = [P.Moves.SelfDestruct, P.Moves.Explosion, P.Moves.MindBlown, P.Moves.MistyExplosion];
+
+export const Damp: Ability = {
+  name: "Damp",
+  description:
+    "The Pokémon dampens its surroundings, preventing all Pokémon from using explosive moves such as Self-Destruct. ",
+  slot: "ability",
+
+  apply: ({ self }) => {
+    function dampSelfDestruct(action: Action<T>) {
+      const { source } = action.params;
+      if (!(source instanceof MoveEntry)) return;
+      // despite the general description, Damp specifically prevents these move:
+      if (!DampMoves.includes(source.effects)) return;
+
+      action.cancel = true;
+    }
+
+    function applyDampToBattle(battle: Battle<T>) {
+      battle.on("action", dampSelfDestruct);
+      self.once("exitBattle", () => battle.off("action", dampSelfDestruct));
+    }
+
+    return {
+      name: Damp.name,
+      activate: () => self.on("enterBattle", applyDampToBattle),
+      deactivate: () => self.off("enterBattle", applyDampToBattle),
+      expiry: permanent,
+    };
+  },
+};
