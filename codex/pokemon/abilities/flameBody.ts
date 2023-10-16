d60069c02723db3d674bc5051fbdfbce7f34664d
Sadie Dotzler <sadiedotzler@gmail.com>
2023-10-15 19:22:55 -0700

    implement flame body

--- codex/pokemon/abilities/flameBody.ts
+++ codex/pokemon/abilities/flameBody.ts
@@ -1,3 +1,43 @@
-import { Ability } from "../mod.ts";
+import loader from "../loader.ts";
+import {
+  Ability,
+  Effects,
+  TargetContext,
+  TraditionalBBP as T,
+  permanent,
+  MoveEntry,
+  effectAction,
+  chance,
+} from "../mod.ts";
 
-export const FlameBody: Ability = {} as Ability;
+export const FlameBody: Ability = loader.register(P => ({
+  name: "Flame Body",
+  description: "Contact with the Pokémon may burn the attacker.",
+  slot: "ability",
+
+  apply: ({ self }) => {
+    function maybeBurnOnContact(_effect: Effects<T>, { action, source, battle }: TargetContext<T>) {
+      if (!(source instanceof MoveEntry)) return;
+      if (!source.effects.makesContact) return;
+
+      action.reactions.add(
+        effectAction({
+          battle,
+          user: self,
+          targets: [source.user],
+          parent: action,
+          effect: {
+            status: chance(3 / 10, P.Statuses.Burn),
+          },
+        })
+      );
+    }
+
+    return {
+      name: FlameBody.name,
+      activate: () => self.on("receiveEffects", maybeBurnOnContact),
+      deactivate: () => self.off("receiveEffects", maybeBurnOnContact),
+      expiry: permanent,
+    };
+  },
+}));
