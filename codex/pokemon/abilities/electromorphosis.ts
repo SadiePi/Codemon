04445f5a9fba538227b6986633bc7cece0b0d78d
Sadie Dotzler <sadiedotzler@gmail.com>
2023-10-15 19:22:48 -0700

    implement electromorphosis

--- codex/pokemon/abilities/electromorphosis.ts
+++ codex/pokemon/abilities/electromorphosis.ts
@@ -1,3 +1,45 @@
-import { Ability } from "../mod.ts";
+import loader from "../loader.ts";
+import { Ability, Effects, MoveEntry, TargetContext, permanent, TraditionalBBP as T, proxy } from "../mod.ts";
 
-export const Electromorphosis: Ability = {} as Ability;
+export const Electromorphosis: Ability = loader.register(P => ({
+  name: "Electromorphosis",
+  description:
+    "The Pokémon becomes charged when it takes damage, boosting the power of the next Electric-type move the Pokémon uses.",
+  slot: "ability",
+
+  apply: ({ self }) => {
+    let charged = false;
+
+    function chargeOnHit(_effect: Effects<T>, { source }: TargetContext<T>) {
+      if (!(source instanceof MoveEntry)) return;
+
+      charged = true;
+    }
+
+    function boostElectricWhenCharged(effect: Effects<T>, { source }: TargetContext<T>) {
+      if (!charged) return;
+      if (!(source instanceof MoveEntry)) return;
+      if (source.effects.type !== P.Types.Electric) return;
+      if (!effect.attack) return;
+
+      effect.attack = proxy(effect.attack, result => {
+        if (!result) return;
+        result.power *= 2;
+      });
+      charged = false;
+    }
+
+    return {
+      name: Electromorphosis.name,
+      activate: () => {
+        self.on("receiveEffects", chargeOnHit);
+        self.on("inflictEffects", boostElectricWhenCharged);
+      },
+      deactivate: () => {
+        self.off("receiveEffects", chargeOnHit);
+        self.off("inflictEffects", boostElectricWhenCharged);
+      },
+      expiry: permanent,
+    };
+  },
+}));
