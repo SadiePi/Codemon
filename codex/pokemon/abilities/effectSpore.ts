480456eef2cd37b7bffc94b8391156bf4da1ed41
Sadie Dotzler <sadiedotzler@gmail.com>
2023-10-26 00:55:40 -0700

    create decider inside builder

--- codex/pokemon/abilities/effectSpore.ts
+++ codex/pokemon/abilities/effectSpore.ts
@@ -1,3 +1,4 @@
+import loader from "../loader.ts";
 import P, {
   Ability,
   TargetContext,
@@ -10,44 +11,46 @@ import P, {
   effectAction,
 } from "../mod.ts";
 
-const STATUS_DECIDER = chance(
-  1 / 3,
-  weighted(
-    { effect: P.Statuses.Poison, weight: 9 },
-    { effect: P.Statuses.Paralysis, weight: 10 },
-    { effect: P.Statuses.Sleep, weight: 11 }
-  )
-);
+export const EffectSpore: Ability = loader.register(P => {
+  const STATUS_DECIDER = chance(
+    1 / 3,
+    weighted(
+      { effect: P.Statuses.Poison, weight: 9 },
+      { effect: P.Statuses.Paralysis, weight: 10 },
+      { effect: P.Statuses.Sleep, weight: 11 }
+    )
+  );
 
-export const EffectSpore: Ability = {
-  name: "Effect Spore",
-  description: "Contact with the Pokémon may inflict poison, sleep, or paralysis on the attacker.",
-  slot: "ability",
+  return {
+    name: "Effect Spore",
+    description: "Contact with the Pokémon may inflict poison, sleep, or paralysis on the attacker.",
+    slot: "ability",
 
-  apply: ({ self }) => {
-    function maybeApplyEffectOnContact(_effect: TargetEffects<T>, { source, action, battle }: TargetContext<T>) {
-      if (!(source instanceof MoveEntry)) return;
-      if (!source.effects.makesContact) return;
-      if (source.user.getSpecies().types.includes(P.Types.Grass)) return;
-      if (source.user.ability === P.Abilities.Overcoat) return;
-      // TODO if source user is holding Safety Goggles, don't apply effect
+    apply: ({ self }) => {
+      function maybeApplyEffectOnContact(_effect: TargetEffects<T>, { source, action, battle }: TargetContext<T>) {
+        if (!(source instanceof MoveEntry)) return;
+        if (!source.effects.makesContact) return;
+        if (source.user.getSpecies().types.includes(P.Types.Grass)) return;
+        if (source.user.ability === P.Abilities.Overcoat) return;
+        // TODO if source user is holding Safety Goggles, don't apply effect
 
-      action.reactions.add(
-        effectAction({
-          battle,
-          user: self,
-          targets: [source.user],
-          parent: action,
-          effect: { status: STATUS_DECIDER },
-        })
-      );
-    }
+        action.reactions.add(
+          effectAction({
+            battle,
+            user: self,
+            targets: [source.user],
+            parent: action,
+            effect: { status: STATUS_DECIDER },
+          })
+        );
+      }
 
-    return {
-      name: EffectSpore.name,
-      activate: () => self.on("receiveEffects", maybeApplyEffectOnContact),
-      deactivate: () => self.off("receiveEffects", maybeApplyEffectOnContact),
-      expiry: permanent,
-    };
-  },
-};
+      return {
+        name: EffectSpore.name,
+        activate: () => self.on("receiveEffects", maybeApplyEffectOnContact),
+        deactivate: () => self.off("receiveEffects", maybeApplyEffectOnContact),
+        expiry: permanent,
+      };
+    },
+  };
+});
