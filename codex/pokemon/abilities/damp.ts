f1a11c426d183132ac67c0e8a7842ce94fac4942
Sadie Dotzler <sadiedotzler@gmail.com>
2023-10-26 00:53:20 -0700

    create move list in builder

--- codex/pokemon/abilities/damp.ts
+++ codex/pokemon/abilities/damp.ts
@@ -1,36 +1,39 @@
-import P, { Ability, TraditionalBBP as T, permanent, MoveEntry, Action, Battle } from "../mod.ts";
+import loader from "../loader.ts";
+import { Ability, TraditionalBBP as T, permanent, MoveEntry, Action, Battle } from "../mod.ts";
 
-// despite the general description, Damp specifically prevents these move:
-const DampMoves = [P.Moves.SelfDestruct, P.Moves.Explosion, P.Moves.MindBlown, P.Moves.MistyExplosion];
+export const Damp: Ability = loader.register(P => {
+  // despite the general description, Damp specifically prevents these move:
+  const DampMoves = [P.Moves.SelfDestruct, P.Moves.Explosion, P.Moves.MindBlown, P.Moves.MistyExplosion];
 
-export const Damp: Ability = {
-  name: "Damp",
-  description:
-    "The Pokémon dampens its surroundings, preventing all Pokémon from using explosive moves such as Self-Destruct. ",
-  slot: "ability",
+  return {
+    name: "Damp",
+    description:
+      "The Pokémon dampens its surroundings, preventing all Pokémon from using explosive moves such as Self-Destruct. ",
+    slot: "ability",
 
-  apply: ({ self }) => {
-    function dampSelfDestruct(action: Action<T>) {
-      const { source } = action.params;
-      if (!(source instanceof MoveEntry)) return;
-      // despite the general description, Damp specifically prevents these move:
-      if (!DampMoves.includes(source.effects)) return;
+    apply: ({ self }) => {
+      function dampSelfDestruct(action: Action<T>) {
+        const { source } = action.params;
+        if (!(source instanceof MoveEntry)) return;
+        // despite the general description, Damp specifically prevents these move:
+        if (!DampMoves.includes(source.effects)) return;
 
-      action.cancel = true;
-    }
+        action.cancel = true;
+      }
 
-    function applyDampToBattle(battle: Battle<T>) {
-      battle.on("action", dampSelfDestruct);
-      self.once("exitBattle", () => battle.off("action", dampSelfDestruct));
-    }
+      function applyDampToBattle(battle: Battle<T>) {
+        battle.on("action", dampSelfDestruct);
+        self.once("exitBattle", () => battle.off("action", dampSelfDestruct));
+      }
 
-    // TODO prevent Aftermath
+      // TODO prevent Aftermath
 
-    return {
-      name: Damp.name,
-      activate: () => self.on("enterBattle", applyDampToBattle),
-      deactivate: () => self.off("enterBattle", applyDampToBattle),
-      expiry: permanent,
-    };
-  },
-};
+      return {
+        name: Damp.name,
+        activate: () => self.on("enterBattle", applyDampToBattle),
+        deactivate: () => self.off("enterBattle", applyDampToBattle),
+        expiry: permanent,
+      };
+    },
+  };
+});
