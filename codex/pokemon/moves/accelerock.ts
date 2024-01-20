27110f09c662b8c18420b312df27c2e34dbb7d42
Sadie Dotzler <sadiedotzler@gmail.com>
2024-01-20 16:43:18 -0700

    implement Accelerock

--- codex/pokemon/moves/accelerock.ts
+++ codex/pokemon/moves/accelerock.ts
@@ -1,4 +1,14 @@
-import { Move } from "../mod.ts";
+import { Move, power } from "../mod.ts";
 import loader from "../loader.ts";
 
-export const Accelerock = {} as Move;
+export const Accelerock: Move = loader.register(P => ({
+  name: "Accelerock",
+  description: "The user smashes into the target at high speed. This move always goes first.",
+  type: P.Types.Rock,
+  category: "Physical",
+  pp: 20,
+  target: { position: "Adjacent", alignment: "Foe" },
+  makesContact: true,
+  priority: 1,
+  attack: power(40),
+}));
