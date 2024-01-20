fae6281872f3ee02e0c462b190ac234d96329365
Sadie Dotzler <sadiedotzler@gmail.com>
2024-01-20 16:43:31 -0700

    implement AcidSpray

--- codex/pokemon/moves/acidSpray.ts
+++ codex/pokemon/moves/acidSpray.ts
@@ -1,4 +1,15 @@
-import { Move } from "../mod.ts";
+import { Move, chance, power } from "../mod.ts";
 import loader from "../loader.ts";
 
-export const AcidSpray = {} as Move;
+export const AcidSpray: Move = loader.register<Move>(P => ({
+  name: "Acid Spray",
+  description: "The user spits fluid that works to melt the target. This harshly lowers the target's Sp. Def stat.",
+  target: { position: "Adjacent", alignment: "Foe" },
+  category: "Special",
+  type: P.Types.Poison,
+  pp: 20,
+  makesContact: false,
+
+  attack: power(40),
+  stages: { specialDefense: -2 },
+}));
