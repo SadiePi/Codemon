5d72ec06514e09e8d8e8ca5e49d6482d8c0d93cc
Sadie Dotzler <sadiedotzler@gmail.com>
2024-01-20 17:04:54 -0700

    partially implement Acrobatics

--- codex/pokemon/moves/acrobatics.ts
+++ codex/pokemon/moves/acrobatics.ts
@@ -1,4 +1,15 @@
-import { Move } from "../mod.ts";
+import { Move, power } from "../mod.ts";
 import loader from "../loader.ts";
 
-export const Acrobatics = {} as Move;
+export const Acrobatics: Move = loader.register<Move>(P => ({
+  name: "Acrobatics",
+  description: "The user nimbly strikes the target. This attack does massive damage if the user isn't holding an item.",
+  type: P.Types.Flying,
+  category: "Physical",
+  pp: 15,
+  target: { position: "Any" },
+  makesContact: true,
+  attack: power(55, (_attack, _context) => {
+    // TODO if(!_context.user.heldItem) _attack.power *= 2;
+  }),
+}));
