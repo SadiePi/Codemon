59c93c1bf01a06826b36f3b7ce7108eef1b04c59
Sadie Dotzler <sadiedotzler@gmail.com>
2024-01-20 17:02:35 -0700

    implement Acupressure

--- codex/pokemon/moves/acupressure.ts
+++ codex/pokemon/moves/acupressure.ts
@@ -1,4 +1,20 @@
-import { Move } from "../mod.ts";
+import { Move, Stats, choose, decide } from "../mod.ts";
 import loader from "../loader.ts";
 
-export const Acupressure = {} as Move;
+export const Acupressure: Move = loader.register<Move>(P => ({
+  name: "Acupressure",
+  description: "The user applies pressure to stress points, sharply boosting one of its or its allies' stats.",
+  type: P.Types.Normal,
+  category: "Status",
+  pp: 30,
+  target: { alignment: "Ally", position: "Adjacent", includeSelf: true },
+  makesContact: false,
+
+  stages: ({ target }) => {
+    const raisableStats = Stats.filter(s => {
+      !target.stats[s].stage.isMax;
+    });
+    const chosenStat = decide(choose(...raisableStats), undefined);
+    return { [chosenStat]: 2 };
+  },
+}));
