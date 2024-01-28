6554398d98b41c70c07c1959966eb6868e0bee7a
Sadie Dotzler <sadiedotzler@gmail.com>
2024-01-27 20:15:17 -0700

    basic methods and tests

--- src/battle/core/team.ts
+++ src/battle/core/team.ts
@@ -1,11 +1,63 @@
 import { BattleBuilderParams, Combatant } from "./mod.ts";
 
-export class Team<P extends BattleBuilderParams<P>> {
-  constructor(public readonly members: Combatant<P>[]) {}
+interface TeamParams<P extends BattleBuilderParams<P>> {
+  members: Combatant<P>[];
+  maxSize?: number | "None";
+}
 
-  public removeMember(member: Combatant<P> | number) {
+export class Team<P extends BattleBuilderParams<P>> {
+  private members: Combatant<P>[];
+  private maxSize: number;
+
+  constructor(params: TeamParams<P>) {
+    if (typeof params.maxSize === "number") this.maxSize = params.maxSize;
+    else this.maxSize = Infinity;
+
+    if (params.members.length > this.maxSize) throw new Error("Too many team members");
+    this.members = params.members;
+  }
+
+  public removeMember(member: Combatant<P> | number): boolean {
+    let index = -1;
+
     if (typeof member === "number") {
-      return !!this.members.splice(member, 1);
-    }
+      if (member >= 0 && member < this.members.length) index = member;
+    } else index = this.members.indexOf(member);
+
+    if (index === -1) return false;
+
+    this.members.splice(index, 1);
+    return true;
+  }
+
+  public canAddMember() {
+    return this.members.length < this.maxSize;
+  }
+
+  public addMember(combatant: Combatant<P>) {
+    if (!this.canAddMember()) return false;
+    if (this.hasMember(combatant)) return false;
+    this.members.push(combatant);
+    return true;
+  }
+
+  public hasMember(combatant: Combatant<P>) {
+    return this.members.includes(combatant);
+  }
+
+  public getMembers() {
+    return [...this.members];
+  }
+
+  public getTeammates(_member: Combatant<P> | number): Combatant<P>[] {
+    return [];
+  }
+
+  public getAdjacent() {
+    return [];
+  }
+
+  public getNonAdjacent() {
+    return [];
   }
 }
--- test/battle/core/team.ts
+++ test/battle/core/team.ts
@@ -1,16 +1,44 @@
+import { assertEquals, iKibble, iBulby } from "../../_common.ts";
 import P from "../../../codex/pokemon/mod.ts";
-import { Team, TraditionalBBP as T, spawn, Codemon } from "../../../src/mod.ts";
+import { Team, TraditionalBBP as T, spawn } from "../../../src/mod.ts";
 
-Deno.test("Team", () => {
-  const members: Codemon[] = [
-    spawn({ species: P.Species.Bulbasaur }),
-    spawn({ species: P.Species.Ivysaur }),
-    spawn({ species: P.Species.Venusaur }),
-    spawn({ species: P.Species.Garchomp }),
-  ];
+Deno.test("Team - Basic Methods", () => {
+  const b = spawn({ species: P.Species.Bulbasaur });
+  const i = spawn({ species: P.Species.Ivysaur });
+  const v = spawn({ species: P.Species.Venusaur });
+  const g = spawn({ species: P.Species.Garchomp });
+  const bulby = spawn(iBulby);
+  const kibble = spawn(iKibble);
 
-  members.forEach(m => console.log(m.name, m.stats.level));
+  const team = new Team<T>({ members: [b, i, v, g], maxSize: 4 });
 
-  const team = new Team<T>(members);
-  team.removeMember(2);
+  assertEquals(team.canAddMember(), false);
+
+  assertEquals(team.removeMember(2), true);
+  assertEquals(team.hasMember(v), false);
+  assertEquals(team.canAddMember(), true);
+  team.getMembers().forEach(m => console.log(m.name, m.stats.level));
+
+  assertEquals(team.removeMember(4), false);
+  assertEquals(team.getMembers().length, 3);
+
+  assertEquals(team.removeMember(0), true);
+  assertEquals(team.hasMember(b), false);
+  assertEquals(team.getMembers().length, 2);
+
+  assertEquals(team.removeMember(g), true);
+  assertEquals(team.hasMember(g), false);
+
+  assertEquals(team.removeMember(g), false);
+
+  assertEquals(team.canAddMember(), true);
+  assertEquals(team.addMember(bulby), true);
+  assertEquals(team.addMember(bulby), false);
+
+  assertEquals(team.addMember(kibble), true);
+  assertEquals(team.getMembers().length, 3);
+
+  assertEquals(team.addMember(b), true);
+  assertEquals(team.addMember(g), false);
+  assertEquals(team.getMembers().length, 4);
 });
