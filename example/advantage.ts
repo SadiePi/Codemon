37b998d1078b32b55a5bf4bb75692e4275268b1c
Sadie Dotzler <sadiedotzler@gmail.com>
2024-02-24 00:13:16 -0700

    remove Deno.test

--- example/advantage.ts
+++ example/advantage.ts
@@ -1,40 +1,40 @@
 import P, { Type } from "../codex/pokemon/mod.ts";
 import { fmt } from "../src/external.ts";
 
-Deno.test("Find Best Type Advantages", () => {
-  const cartesianProduct = <T, U>(array1: T[], array2: U[]): [T, U][] =>
-    array1.flatMap(a1 => array2.map(a2 => [a1, a2] as [T, U]));
+// I'm not sure how useful this kind of comparison actually is, but this just shows how it's possible
 
-  function getTypeAdvantage(attacking: Type, defending: Type): number {
-    if (defending.immunities.includes(attacking)) return 0;
-    if (defending.resistances.includes(attacking)) return 0.5;
-    if (defending.weaknesses.includes(attacking)) return 2;
-    return 1;
-  }
+const cartesianProduct = <T, U>(array1: T[], array2: U[]): [T, U][] =>
+  array1.flatMap(a1 => array2.map(a2 => [a1, a2] as [T, U]));
 
-  const getPairAdvantage = (attackingPair: [Type, Type], defendingPair: [Type, Type]) =>
-    cartesianProduct(attackingPair, defendingPair)
-      .map(([attackingType, defendingType]) => getTypeAdvantage(attackingType, defendingType))
-      .reduce((p, c) => p * c);
+function getTypeAdvantage(attacking: Type, defending: Type): number {
+  if (defending.immunities.includes(attacking)) return 0;
+  if (defending.resistances.includes(attacking)) return 0.5;
+  if (defending.weaknesses.includes(attacking)) return 2;
+  return 1;
+}
 
-  const getTotalPairAdvantage = (pair: [Type, Type], against: [Type, Type][]) =>
-    against
-      .map(defendingPair => getPairAdvantage(pair, defendingPair) - getPairAdvantage(defendingPair, pair))
-      .reduce((p, c) => p + c);
+const getPairAdvantage = (attackingPair: [Type, Type], defendingPair: [Type, Type]) =>
+  cartesianProduct(attackingPair, defendingPair)
+    .map(([attackingType, defendingType]) => getTypeAdvantage(attackingType, defendingType))
+    .reduce((p, c) => p * c);
 
-  const NoneType: Type = { name: "None", immunities: [], resistances: [], weaknesses: [], color: 0x000000 };
+const getTotalPairAdvantage = (pair: [Type, Type], against: [Type, Type][]) =>
+  against
+    .map(defendingPair => getPairAdvantage(pair, defendingPair) - getPairAdvantage(defendingPair, pair))
+    .reduce((p, c) => p + c);
 
-  const types = [...Object.values(P.Types), NoneType];
-  const typePairings = cartesianProduct(types, types).filter(([t1, t2]) => t1.name < t2.name);
+const NoneType: Type = { name: "None", immunities: [], resistances: [], weaknesses: [], color: 0x000000 };
 
-  const effectivenesses = typePairings.map(attackingPair => ({
-    pair: attackingPair,
-    effectiveness: getTotalPairAdvantage(attackingPair, typePairings),
-  }));
+const types = [...Object.values(P.Types), NoneType];
+const typePairings = cartesianProduct(types, types).filter(([t1, t2]) => t1.name < t2.name);
 
-  effectivenesses.sort((a, b) => b.effectiveness - a.effectiveness);
+const effectivenesses = typePairings.map(attackingPair => ({
+  pair: attackingPair,
+  effectiveness: getTotalPairAdvantage(attackingPair, typePairings),
+}));
 
-  effectivenesses.forEach(({ pair: [type1, type2], effectiveness }) =>
-    console.log(`${fmt.rgb24(type1.name, type1.color)}/${fmt.rgb24(type2.name, type2.color)}: ${effectiveness}`)
-  );
-});
+effectivenesses.sort((a, b) => b.effectiveness - a.effectiveness);
+
+effectivenesses.forEach(({ pair: [type1, type2], effectiveness }) =>
+  console.log(`${fmt.rgb24(type1.name, type1.color)}/${fmt.rgb24(type2.name, type2.color)}: ${effectiveness}`)
+);
