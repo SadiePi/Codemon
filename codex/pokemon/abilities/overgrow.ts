64b702857e17de197f81fe7bda05624fca691a2a
Sadie Dotzler <sadiedotzler@gmail.com>
2024-03-10 00:28:01 -0700

    revamp implementation

--- codex/pokemon/abilities/overgrow.ts
+++ codex/pokemon/abilities/overgrow.ts
@@ -1,13 +1,4 @@
-import P, {
-  Ability,
-  Battle,
-  Effects,
-  MoveEntry,
-  TraditionalBBP as T,
-  TargetContext,
-  decide,
-  permanent,
-} from "../mod.ts";
+import P, { Ability, Effects, MoveEntry, TraditionalBBP as T, TargetContext, permanent, proxy } from "../mod.ts";
 
 export const Overgrow: Ability = {
   name: "Overgrow",
@@ -16,37 +7,22 @@ export const Overgrow: Ability = {
 
   apply: ({ self }) => {
     const boostGrassMovesWhenLowHP = (effect: Effects<T>, context: TargetContext<T>) => {
-      const { source } = context;
-      if (!(source instanceof MoveEntry)) return;
-      if (source.user !== self) return;
-      if (source.effects.type !== P.Types.Grass) if (source.user !== self) return;
-      if (!effect.attack) return;
       if (self.stats.hp.percent > 1 / 3) return;
 
-      const old = effect.attack;
-      effect.attack = () => {
-        const attack = decide(old, context);
-        if (!attack) return undefined;
-        attack.stat = attack.stat * 1.5;
-        return attack;
-      };
-    };
+      const { source } = context;
+      if (!(source instanceof MoveEntry)) return;
+      if (source.effects.type !== P.Types.Grass) return;
+      if (!effect.attack) return;
 
-    const startListeningForEffects = (battle: Battle<T>) => battle.on("effect", boostGrassMovesWhenLowHP);
-    const stopListeningForEffects = (battle: Battle<T>) => battle.off("effect", boostGrassMovesWhenLowHP);
+      effect.attack = proxy(effect.attack, result => {
+        if (result) result.stat = result.stat * 1.5;
+      });
+    };
 
     return {
       name: Overgrow.name,
-      activate: () => {
-        self.on("enterBattle", startListeningForEffects);
-        self.on("exitBattle", stopListeningForEffects);
-      },
-      deactivate: () => {
-        self.off("enterBattle", startListeningForEffects);
-        self.off("exitBattle", stopListeningForEffects);
-
-        // TODO battle.off somehow
-      },
+      activate: () => self.on("inflictEffects", boostGrassMovesWhenLowHP),
+      deactivate: () => self.off("inflictEffects", boostGrassMovesWhenLowHP),
       expiry: permanent,
     };
   },
