bdec2701f1f3ed6ae94da3ec5835e528a9940fdb
Sadie Dotzler <sadiedotzler@gmail.com>
2024-06-01 23:20:22 -0700

    better implementation

--- example/dumpCodex.ts
+++ example/dumpCodex.ts
@@ -1,13 +1,7 @@
-import C from "../codex/pokemon/mod.ts";
+import Pokedex from "../codex/pokemon/mod.ts";
 
-// Hmm... this probably needs better support
-
-for (const codexKey of Object.keys(C)) {
-  console.log(codexKey);
-  for (const entryKey of Object.keys(C[codexKey as keyof typeof C])) {
-    // deno-lint-ignore ban-types
-    const entry = C[codexKey as keyof typeof C][entryKey as keyof (typeof C)[keyof typeof C]] as {}; // jfc
+for (const [categoryKey, category] of Object.entries(Pokedex))
+  for (const [entryKey, entry] of Object.entries(category)) {
     if (typeof entry === "object" && Object.keys(entry).length === 0) continue;
-    console.log(`\t${entryKey}`);
+    console.log(`${categoryKey}: ${entryKey}`);
   }
-}
